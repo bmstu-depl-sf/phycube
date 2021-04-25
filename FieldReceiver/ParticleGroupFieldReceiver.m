1522973cdb3d504e646f4e3384d6d4b5e2d6fec0
Kanena <kobernik-t-n@yandex.ru>
2021-04-25 15:23:23 +0300

    Receiver

--- FieldReceiver/ParticleGroupFieldReceiver.m
+++ FieldReceiver/ParticleGroupFieldReceiver.m
@@ -1,24 +1,17 @@
 classdef ParticleGroupFieldReceiver < FieldReceiver
     properties
-        receiver(1, 1) Particle = Particle.empty()
-        creators(1, :) FiledCreatedByParticles = FiledCreatedByParticles.empty()
-        %ResultForce(1, 1) Vec3D = Vec3D.empty()
+        receiver(1, :) Particle = Particle.empty()
+        creator(1, 1) FiledCreatedByParticles = FiledCreatedByParticles.empty()
     end
     methods
         function calculateResultForce(obj)
            ElForce = ElectricForce();
-           ResultForce = Vec3D.empty();
-           F = Vec3D.empty();
-           for i = 1:size(obj.creators,2)
-               for j = 1:size(obj.creators.particles, 2)
-                   if(obj.receiver.coord ~= obj.creators(i).particles(j).coord)
-                      ElForce = ElForce.calculateForce(obj.creators.field, obj.receiver, obj.creators(i).particles(j).coord);
-                      F.coord = ElForce.Force;
-                      ResultForce = plus(ResultForce, F);
-                   end
+           for i = 1:size(obj.receiver,2)
+               if(output(obj.receiver(i)) ~= output(obj.creators.particles))
+                  ElForce = ElForce.calculateForce(obj.creator.field, obj.receiver(i), output(obj.creator.particles));
+                  obj.receiver(i).R = obj.receiver(i).R + ElForce;
                end
            end
-           obj.receiver.R = ResultForce;
         end
     end
 end
